fc2e3cea2272cb36ddc27177da37c12688ed50c1
hammadshaikh-22 <hammadshaikh1h23@gmail.com>
2025-09-28 05:22:26 -0700

    i

--- app.js
+++ app.js
@@ -1,30 +1,30 @@
 row = 3         //+prompt("Enter the number of rows: ")
 col = 3         //+prompt("Enter the number of columns: ")
 
-// for (var i = 0; i < row; i++) {
-//     var a = ""
-//     for (var j = 0; j < col; j++) {
-//         a = a + "*"
+for (var i = 0; i < row; i++) {
+    var a = ""
+    for (var j = 0; j < col; j++) {
+        a = a + "*"
 
-//     }
-//     console.log(a)
+    }
+    console.log(a)
 
 
-// }
+}
 
-// var b=1
-// for (var i = 0; i < 3; i++) {
-//     var a=""
-//     for(var j = 0; j < 3; j++){
-//         a=a+b
-//         b++
+var b=1
+for (var i = 0; i < 3; i++) {
+    var a=""
+    for(var j = 0; j < 3; j++){
+        a=a+b
+        b++
         
-//     }
-//     console.log(a)
+    }
+    console.log(a)
      
 
     
-// }
+}
 
 var a = ""
 for (var i = 0; i < 5; i++) {
