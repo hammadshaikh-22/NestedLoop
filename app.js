6e544168f5583dcd92cbb81926f8d22bb3bb8f1f
hammadshaikh-22 <hammadshaikh1h23@gmail.com>
2025-09-28 05:01:50 -0700

    i

--- app.js
+++ app.js
@@ -1,10 +1,27 @@
-for (var i = 0; i < 2; i++) {
+// row = 3         //+prompt("Enter the number of rows: ")
+// col = 3         //+prompt("Enter the number of columns: ")
+
+// for (var i = 0; i < row; i++) {
+//     var a = ""
+//     for (var j = 0; j < col; j++) {
+//         a = a + "*"
+
+//     }
+//     console.log(a)
+
+
+// }
+
+var b=0
+for (var i = 0; i < 3; i++) {
     var a=""
-    for(var j=0;j<2;j++){
-        a=a+"*"
+    for(var j = 0; j < 3; j++){
+        a+= b+1
+        b++
         
     }
     console.log(a)
-    
+     
+
     
 }
