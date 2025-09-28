f4cf99f41bf7bd9e1a5c51e6d06394980964f178
hammadshaikh-22 <hammadshaikh1h23@gmail.com>
2025-09-28 05:36:26 -0700

    i

--- app.js
+++ app.js
@@ -1,30 +1,30 @@
-// row = 3         //+prompt("Enter the number of rows: ")
-// col = 3         //+prompt("Enter the number of columns: ")
+row = 3         //+prompt("Enter the number of rows: ")
+col = 3         //+prompt("Enter the number of columns: ")
 
-// for (var i = 0; i < row; i++) {
-//     var a = ""
-//     for (var j = 0; j < col; j++) {
-//         a = a + "*"
+for (var i = 0; i < row; i++) {
+    var a = ""
+    for (var j = 0; j < col; j++) {
+        a = a + "*"
 
-//     }
-//     console.log(a)
+    }
+    console.log(a)
 
 
-// }
+}
 
-// var b=1
-// for (var i = 0; i < 3; i++) {
-//     var a=""
-//     for(var j = 0; j < 3; j++){
-//         a=a+b
-//         b++
+var b=1
+for (var i = 0; i < 3; i++) {
+    var a=""
+    for(var j = 0; j < 3; j++){
+        a=a+b
+        b++
         
-//     }
-//     console.log(a)
+    }
+    console.log(a)
      
 
     
-// }
+}
 
 var a = ""
 for (var i = 0; i < 4; i++) {
@@ -37,16 +37,16 @@ for (var i = 0; i < 4; i++) {
 
 }
 
-// var b=3
-// for (var i = 0; i < 4; i++) {
-//     var a=""
-//     for(var j = 0; j < 3; j++){
-//         a=a+" "+b
-//         b+=3
+var b=3
+for (var i = 0; i < 4; i++) {
+    var a=""
+    for(var j = 0; j < 3; j++){
+        a=a+" "+b
+        b+=3
         
-//     }
-//     console.log(a)
+    }
+    console.log(a)
      
 
     
-// }
+}
